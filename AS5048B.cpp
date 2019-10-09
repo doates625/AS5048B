e4cc4efa1544589893c42a723a566ed4df1a6e57
Dan <doates625@gmail.com>
2019-10-08 22:47:42 -0400

    Tested both Arduino and Mbed builds

---
+++ AS5048B.cpp
@@ -0,0 +1,61 @@
+/**
+ * @file AS5048B.cpp
+ * @author Dan Oates (WPI Class of 2020)
+ */
+#include "AS5048B.h"
+#include <CppUtil.h>
+#include <math.h>
+
+/**
+ * Static Constants
+ */
+const float AS5048B::rad_per_cnt = 2.0f * M_PI / 16384.0f;
+
+/**
+ * @brief Constructor for AS5048B encoder interface
+ * @param i2c Platform-specific I2C interface
+ * @param i2c_addr I2C address (default 0x40)
+ * @param home_angle Zero angle (default 0)
+ */
+AS5048B::AS5048B(I2CDEVICE_I2C_CLASS* i2c, uint8_t i2c_addr, float home_angle)
+{
+	this->i2c = I2CDevice(i2c, i2c_addr, I2CDevice::msb_first);
+	this->home_angle = home_angle;
+}
+
+/**
+ * @brief Default constructor for AS5048B encoder.
+ * 
+ * Note: For array instantiation only.
+ */
+AS5048B::AS5048B()
+{
+	this->i2c = I2CDevice();
+	this->home_angle = 0.0f;
+}
+
+/**
+ * @brief Sets home angle to current angle.
+ */
+void AS5048B::set_home()
+{
+	home_angle = 0.0f;
+	home_angle = get_angle();
+}
+
+/**
+ * @brief Returns angle relative to home in radians [-pi, pi].
+ */
+float AS5048B::get_angle()
+{
+	// Get MSB and LSB from device
+	i2c.read_sequence(reg_angle_addr, 2);
+	uint16_t msb = i2c.read_uint8();
+	uint16_t lsb = i2c.read_uint8();
+	uint16_t angle_raw = (msb << 6) | lsb;
+
+	// Convert to float and wrap to [-pi, pi]
+	float angle = angle_raw * rad_per_cnt;
+	angle = mod_limit(angle - home_angle, -M_PI, +M_PI);
+	return angle;
+}
---
+++ AS5048B.h
@@ -0,0 +1,38 @@
+/**
+ * @file AS5048B.h
+ * @brief Class for interfacing with AS5048B 14-bit I2C encoder.
+ * @author Dan Oates (WPI Class of 2020)
+ * 
+ * The AS5048B is a 14-bit encoder which supports both I2C and
+ * quadrature output. This class provides a simple interface for
+ * reading the angle and calibrating a home (zero) angle.
+ * 
+ * Dependencies:
+ * - I2CDevice: https://github.com/doates625/I2CDevice.git
+ * - Platform: https://github.com/doates625/Platform.git
+ * - Unions: https://github.com/doates625/Unions.git
+ * - CppUtil: https://github.com/doates625/CppUtil.git
+ * 
+ * References:
+ * - Datasheet: https://www.mouser.com/ds/2/588/AS5048_DS000298_3-00-522551.pdf
+ */
+
+#pragma once
+#include <I2CDevice.h>
+
+class AS5048B
+{
+public:
+	AS5048B(
+		I2CDEVICE_I2C_CLASS* i2c,
+		uint8_t i2c_addr = 0x40,
+		float home_angle = 0.0f);
+	AS5048B();
+	void set_home();
+	float get_angle();
+private:
+	static const uint8_t reg_angle_addr = 0xFE;
+	static const float rad_per_cnt;
+	I2CDevice i2c;
+	float home_angle;
+};
