2ce11472224b8f04839da880575aec9968ae134b
Dan <doates625@gmail.com>
2019-10-18 12:32:50 -0700

    Removed description from header file

--- AS5048B.h
+++ AS5048B.h
@@ -2,19 +2,6 @@
  * @file AS5048B.h
  * @brief Class for interfacing with AS5048B 14-bit I2C encoder
  * @author Dan Oates (WPI Class of 2020)
- * 
- * The AS5048B is a 14-bit encoder which supports I2C and quadrature output.
- * This class provides a simple I2C interface for reading the angle and
- * calibrating a home (zero) angle.
- * 
- * Dependencies:
- * - I2CDevice: https://github.com/doates625/I2CDevice.git
- * - Platform: https://github.com/doates625/Platform.git
- * - Unions: https://github.com/doates625/Unions.git
- * - CppUtil: https://github.com/doates625/CppUtil.git
- * 
- * References:
- * - Datasheet: https://www.mouser.com/ds/2/588/AS5048_DS000298_3-00-522551.pdf
  */
 #pragma once
 #include <I2CDevice.h>
